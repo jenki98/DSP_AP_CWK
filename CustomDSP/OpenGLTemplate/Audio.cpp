b639e15a64f5667c7059468892475400951dc18d
jenki98 <zaynab.bawa@gmail.com>
2022-04-10 15:31:23 +0100

    CUSTOM DSP + CIRCULAR BUFFER ADDED

---
+++ CustomDSP/OpenGLTemplate/Audio.cpp
@@ -0,0 +1,137 @@
+#include "Audio.h"
+
+#pragma comment(lib, "lib/fmod_vc.lib")
+
+/*
+I've made these two functions non-member functions
+*/
+
+// Check for error
+void FmodErrorCheck(FMOD_RESULT result)
+{
+	if (result != FMOD_OK) {
+		const char *errorString = FMOD_ErrorString(result);
+		// MessageBox(NULL, errorString, "FMOD Error", MB_OK);
+		// Warning: error message commented out -- if headphones not plugged into computer in lab, error occurs
+	}
+}
+
+// DSP callback
+FMOD_RESULT F_CALLBACK DSPCallback(FMOD_DSP_STATE *dsp_state, float *inbuffer, float *outbuffer, unsigned int length, int inchannels, int *outchannels)
+{
+	FMOD::DSP *thisdsp = (FMOD::DSP *)dsp_state->instance;
+
+	for (unsigned int samp = 0; samp < length; samp++)
+	{
+		for (int chan = 0; chan < *outchannels; chan++)
+		{
+			/*
+			This DSP filter just halves the volume!
+			Input is modified, and sent to output.
+			*/
+			outbuffer[(samp * *outchannels) + chan] = inbuffer[(samp * inchannels) + chan] * 0.2f;
+		}
+	}
+
+	return FMOD_OK;
+}
+
+CAudio::CAudio()
+{}
+
+CAudio::~CAudio()
+{}
+
+bool CAudio::Initialise()
+{
+	// Create an FMOD system
+	result = FMOD::System_Create(&m_FmodSystem);
+	FmodErrorCheck(result);
+	if (result != FMOD_OK) 
+		return false;
+
+	// Initialise the system
+	result = m_FmodSystem->init(32, FMOD_INIT_NORMAL, 0);
+	FmodErrorCheck(result);
+	if (result != FMOD_OK) 
+		return false;
+
+
+
+	// Create the DSP effect
+	{
+		FMOD_DSP_DESCRIPTION dspdesc;
+		memset(&dspdesc, 0, sizeof(dspdesc));
+
+		strncpy_s(dspdesc.name, "My first DSP unit", sizeof(dspdesc.name));
+		dspdesc.numinputbuffers = 1;
+		dspdesc.numoutputbuffers = 1;
+		dspdesc.read = DSPCallback;
+
+		result = m_FmodSystem->createDSP(&dspdesc, &m_dsp);
+		FmodErrorCheck(result);
+
+		if (result != FMOD_OK)
+			return false;
+	}
+
+	return true;
+	
+}
+
+// Load an event sound
+bool CAudio::LoadEventSound(char *filename)
+{
+	result = m_FmodSystem->createSound(filename, FMOD_LOOP_OFF, 0, &m_eventSound);
+	FmodErrorCheck(result);
+	if (result != FMOD_OK) 
+		return false;
+
+	return true;
+	
+
+}
+
+// Play an event sound
+bool CAudio::PlayEventSound()
+{
+	result = m_FmodSystem->playSound(m_eventSound, NULL, false, NULL);
+	FmodErrorCheck(result);
+	if (result != FMOD_OK)
+		return false;
+	return true;
+}
+
+
+// Load a music stream
+bool CAudio::LoadMusicStream(char *filename)
+{
+	result = m_FmodSystem->createStream(filename, NULL | FMOD_LOOP_NORMAL, 0, &m_music);
+	FmodErrorCheck(result);
+
+	if (result != FMOD_OK) 
+		return false;
+
+	return true;
+	
+
+}
+
+// Play a music stream
+bool CAudio::PlayMusicStream()
+{
+	result = m_FmodSystem->playSound(m_music, NULL, false, &m_musicChannel);
+	FmodErrorCheck(result);
+
+	if (result != FMOD_OK)
+		return false;
+
+	m_musicChannel->addDSP(0, m_dsp);
+
+	return true;
+}
+
+void CAudio::Update()
+{
+	m_FmodSystem->update();
+}
