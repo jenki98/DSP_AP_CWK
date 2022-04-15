4967a3ea487521d9e66175310a29a46c82dc50f8
jenki98 <zaynab.bawa@gmail.com>
2022-04-15 16:54:29 +0100

    doppler effect added

--- Task2a/OpenGLTemplate/Audio.h
+++ Task2a/OpenGLTemplate/Audio.h
@@ -43,6 +43,8 @@ private:
 	float m_musicVolume = 0.2f;
 	FMOD_VECTOR m_ballpos;
 	FMOD_VECTOR camPos;
+	FMOD_VECTOR posVel;
+	glm::vec3 vel = glm::vec3(4.0f);
 	float m_doppler;
 	float m_speed;
 };
